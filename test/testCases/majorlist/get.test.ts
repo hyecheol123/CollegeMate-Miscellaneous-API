1895a15c26236394233ebe9312543b4cbc04df65
Hyecheol (Jerry) Jang <hyecheol123@gmail.com>
2023-05-28 23:40:33 -0500

    Add test cases (TODOs)

--- test/testCases/majorlist/get.test.ts
+++ test/testCases/majorlist/get.test.ts
@@ -6,9 +6,8 @@
 
 // eslint-disable-next-line node/no-unpublished-import
 import * as request from 'supertest';
-import TestEnv from '../TestEnv';
-import ExpressServer from '../../src/ExpressServer';
-import * as Cosmos from '@azure/cosmos';
+import TestEnv from '../../TestEnv';
+import ExpressServer from '../../../src/ExpressServer';
 
 describe('GET /majorlist - Get Major List', () => {
   let testEnv: TestEnv;
@@ -34,15 +33,35 @@ describe('GET /majorlist - Get Major List', () => {
     // Request - With wrong Origin Header
     response = await request(testEnv.expressServer.app)
       .get('/majorlist')
-      .set({Origin: 'https://suspicious.com'});
+      .set({ Origin: 'https://suspicious.com' });
     expect(response.status).toBe(403);
     expect(response.body.error).toBe('Forbidden');
 
     // Request - With wrong applicationKey Header
     response = await request(testEnv.expressServer.app)
       .get('/majorlist')
-      .set({'X-APPLICATION-KEY': '<Suspicious-App>'});
+      .set({ 'X-APPLICATION-KEY': '<Suspicious-App>' });
     expect(response.status).toBe(403);
     expect(response.body.error).toBe('Forbidden');
+  });
+
+  test('Fail - Request without body', async () => {
+    fail();
+  });
+
+  test('Fail - Request with additional properties', async () => {
+    fail();
+  });
+
+  test('Fail - Request with not existing school property', async () => {
+    fail();
+  });
+
+  test('Success - Request from collegemate.app', async () => {
+    fail();
+  });
+
+  test('Success - Request from Application', async () => {
+    fail();
   });
 });
