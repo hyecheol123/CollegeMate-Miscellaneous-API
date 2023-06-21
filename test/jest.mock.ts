8a556f286b59f13a0a3053ad259f933d0f9151d9
Seok-Hee (Steve) Han <hihidoublehi@gmail.com>
2023-06-21 15:51:19 +0900

    [Misc API] - MajorListCrawler mock finished

---
+++ test/jest.mock.ts
@@ -0,0 +1,27 @@
+/**
+ * File to execute when jest test environmet is started.
+ * Mocking crawlMajorList module
+ *
+ * @author Seok-Hee (Steve) Han <seokheehan01@gmail.com>
+ */
+
+// Major List Mock Data
+jest.mock('../src/functions/majorListCrawler/crawlMajorList', () => ({
+  __esModule: true,
+  default: jest.fn(async () => {
+    return {
+      major: [
+        'Computer Science',
+        'Chemistry',
+        'Physics',
+        'Mathematics',
+        'Biology',
+        'Economics',
+        'Psychology',
+        'English',
+        'History',
+        'Sociology',
+      ],
+    };
+  }),
+}));
