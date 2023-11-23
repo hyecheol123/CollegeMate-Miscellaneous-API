cb1ef3fb0cb7f8f0fb6aad5ee3e13ddee78363c1
Hyecheol (Jerry) Jang <hyecheol123@gmail.com>
2023-11-23 09:48:24 -0600

    Update Dependencies and Comments

--- src/datatypes/termsAndCondition/TNCResponseObj.ts
+++ src/datatypes/termsAndCondition/TNCResponseObj.ts
@@ -3,8 +3,8 @@
  *  - version
  *  - createdAt
  *  - content:
- *    - pa
- *    - tnc
+ *    - privacyAct
+ *    - termsAndConditions
  *
  * @author Hyecheol (Jerry) Jang <hyecheol123@gmail.com>
  * @author Seok-Hee (Steve) Han <seokheehan01@gmail.com>
